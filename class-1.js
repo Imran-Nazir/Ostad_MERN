fb25bf3e768a5adaef2742a2f00bbebb3ba4dcf4
Imran-Nazir <55039265+Imran-Nazir@users.noreply.github.com>
2023-07-26 14:43:12 +0600

    ha

--- class-1.js
+++ class-1.js
@@ -1,3 +1,4 @@
 console.log('Hello');
 console.log('My name is Md. Imran Nazir.');
-console.log('I love javascript.')
+console.log('I love javascript.');
+console.log('I have to finish my MERN stack journey.');
